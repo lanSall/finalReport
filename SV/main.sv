ddf1413816a06f3041ff6b93a7f3e363b56aab63
lanSall <158085285+lanSall@users.noreply.github.com>
2024-04-22 12:27:39 -0500

    yes

--- SV/main.sv
+++ SV/main.sv
@@ -1,33 +1,32 @@
-module main(clk, reset, start, randomize, grid);
+module main(clk, reset, start, randomize, muxseed, grid);
     input logic clk;
     input logic reset;
     input logic start;
     input logic randomize;
+    input logic [63:0] muxseed;
 
     logic rst;
     logic strt;
     logic rnd;
+
     logic [63:0]grid_evolve;
-    logic [63:0]seed;
     logic [63:0]lfsrout;
-    logic [63:0]flopout;
+    logic [63:0]datapathout;
 
     output logic [63:0] grid;
     
-    always@(*) 
+    always@(*)
         case({rst,rnd,strt})
-        3'b100 : seed <=64'b0000000000000000000000000000000011100000000000000000000000000000;
-        3'b010 : seed <=lfsrout;
-        3'b001 : seed <=grid;
+        3'b100 : grid <= muxseed;
+        3'b010 : grid <= lfsrout;
+        3'b001 : grid <= datapathout;
+        default: grid <= 64'b0000000000111000000000000000000000000000000000000000000000000000;
         endcase
-        // to implement this as a with multiple values chagne the reset value to a holder variable and create a case statement used to assing values to it
-    
+        
+    //set is a mux for inputing our seeds
 
     FSM dut1(clk, reset, start, randomize, rst, strt, rnd);
-
-    flopenr #(64) dut2(clk, rst, strt, seed, grid, grid_evolve);
-    datapath dut3(grid_evolve, grid);
-
-    flopenr #(64) dut(clk, rst, strt, seed, lfsrout, flopout);
-    lfsr64 dut4(clk, rst, flopout, lfsrout);
+    flopenr #(64) dut2(clk, grid, grid_evolve);
+    datapath dut3(grid_evolve, datapathout);
+    lfsr64 dut4(clk, rst, grid_evolve, lfsrout);
 endmodule
